5a758c8f7aa280367424ed3162949bff05da57c8
kingoflmht1502 <manhphuquynguyen@gmail.com>
2023-10-24 16:47:49 +0700

    Add new Food page

---
+++ src/client/pages/Food.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+const Food = () => {
+	return (
+		<Layout>
+			<div>Food</div>
+		</Layout>
+	);
+};
+
+export default Food;
