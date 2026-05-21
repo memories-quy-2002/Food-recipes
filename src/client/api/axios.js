1c865b24978ca6743da6c38b38f2fdf7ffcca929
MemoriesQ <manhphuquynguyen@gmail.com>
2026-05-21 16:10:08 +0700

    Update API base URL handling for environment

--- src/client/api/axios.js
+++ src/client/api/axios.js
@@ -2,10 +2,9 @@ import axios from "axios";
 
 const localApiBaseUrl = "http://localhost:4000";
 const productionApiBaseUrl = "https://food-recipes-server-omega.vercel.app";
-const apiBaseUrl =
-	process.env.NODE_ENV === "production"
-		? productionApiBaseUrl
-		: localApiBaseUrl
+const apiBaseUrl = import.meta.env.PROD
+	? productionApiBaseUrl
+	: localApiBaseUrl;
 
 export default axios.create({
 	baseURL: apiBaseUrl,
