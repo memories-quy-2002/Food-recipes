33fd16615db85b7193b3c3b03d4bea46492a48bd
memories-quy-2002 <manhphuquynguyen@gmail.com>
2026-05-21 16:20:04 +0700

    feat: remove local and production API base URL checks, using environment variable directly

--- src/client/api/axios.js
+++ src/client/api/axios.js
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-const localApiBaseUrl = "http://localhost:4000";
-const productionApiBaseUrl = "https://food-recipes-server-omega.vercel.app";
-const apiBaseUrl = import.meta.env.PROD
-	? productionApiBaseUrl
-	: localApiBaseUrl;
-
 export default axios.create({
-	baseURL: apiBaseUrl,
+	baseURL: import.meta.env.VITE_API_BASE_URL,
 });
